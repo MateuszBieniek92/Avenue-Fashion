08532a63105d27190fe5991fd7a9c9474bf56c07
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2018-02-17 12:04:01 +0100

    Modiefied classes to BEM metodology

--- js/application.js
+++ js/application.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    // hover effect most-popular-section
+    // hover effect most__popular__section
     
-    var ClothPlacingSmall = document.getElementsByClassName('cloth-placing-small');
-    var ClothPlacingBig = document.getElementsByClassName('cloth-placing');
+    var ClothPlacingSmall = document.getElementsByClassName('cloth__placing__small');
+    var ClothPlacingBig = document.getElementsByClassName('cloth__placing');
 
     function clothShadowHover(name) {
         for (var i = 0; i < name.length; i++) {
             name[i].onmouseover = function () {
-                this.classList.add("cloth-hover-shadow");
+                this.classList.add("cloth__hover__shadow");
             };
             name[i].onmouseleave = function () {
-                this.classList.remove("cloth-hover-shadow");
+                this.classList.remove("cloth__hover__shadow");
             };
         }
     }
